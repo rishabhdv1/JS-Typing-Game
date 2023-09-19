6d8b70fb43b449da3b2109160b4598d185b52b8d
rishabhdv1 <rishabh.bairagi.136@gmail.com>
2023-09-19 14:08:49 +0530

    new

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+
+let saveRegistrationInfo = () =>{
+    console.log("jhfhieincjhi");
+            //object.method()
+    let fn = document.getElementById("first_name").value;
+    let ln = document.getElementById("last_name").value;
+
+
+    console.log(fn);
+    console.log(ln);
+    window.localStorage.setItem('first_name',fn)
+    window.localStorage.setItem('last_name',ln)
+    //Page reload
+    window.location.reload();
+}
+
+let logout = () =>{
+    window.localStorage.clear();
+
+    //Page reload
+    window.location.reload();
+}
+
+// ()(); IIFE Immediatelt Invoked Function Expression
+(()=>{
+    let fn = window.localStorage.getItem('first_name')
+    let ln = window.localStorage.getItem('last_name')
+    console.log('Page loaded successfully');
+    // Get a reference to the modal element
+    var modal = document.getElementById('registrationModal');
+
+    // Create a Bootstrap modal instance using the modal element
+    var modalInstance = new bootstrap.Modal(modal);
+
+    // Call the `show` method on the modal instance to
+    console.log(fn);
+    if(window.localStorage.getItem('first_name') === null ){
+        //True
+        modalInstance.show();
+    }
+    // Check if the localStorage is set or not
+    if(fn !== null){
+        document.querySelector('.r_welcome').innerHTML = 'Welcome '+fn +" "+ln + '<button class="btn btn-sm btn-danger mt-3 me-3" onclick="logout()" >Log Out</button>';
+    }
+
+
+
+})();
